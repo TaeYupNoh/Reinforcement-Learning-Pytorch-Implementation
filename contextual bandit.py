f048d8ea253e980b6f21f1b6e1511a18b038ca45
yeop <shxoduq2@naver.com>
2023-03-08 17:26:19 +0900

    Revise CB code

--- contextual bandit.py
+++ contextual bandit.py
@@ -5,70 +5,99 @@ import torch.nn as nn
 import torch.optim as optim
 import numpy as np
 
-
+# Define contextual bandit
 class ContextualBandit:
     def __init__(self):
-        self.state = np.array([[1.0, 2.0, 3.0, 4.0],
-                               [4.0, 3.0, 2.0, 1.0],
-                               [1.0, 2.0, 3.0, 4.0]])
-        self.n_bandits, self.n_features = self.state.shape
-        self.n_actions = 3
+        self.state = 0
+        self.bandits = np.array([[0.2,0,-0.0,-5], 
+                                 [0.1,-5,1,0.25], 
+                                 [-5,5,5,5]])
+        self.num_bandits = self.bandits.shape[0]
+        self.num_actions = self.bandits.shape[1]
+        
+    def get_state(self):
+        self.state = np.random.randint(0, self.num_bandits)
+        return self.state
+    
+    def pull_arm(self, action):
+        bandit = self.bandits[self.state, action]
+        result = np.random.randn(1)
+        if result > bandit:
+            return 1
+        else:
+            return -1
 
-        self.correct_bandit = np.random.randint(self.n_bandits)
-
-    def get_reward(self, action):
-        reward = 0
-        if action == self.correct_bandit:
-            reward = 1.0
-        return reward
-
-
+# Define policy
 class Policy(nn.Module):
-    def __init__(self):
+    def __init__(self, num_features, num_actions, hidden_size):
         super(Policy, self).__init__()
-        self.fc1 = nn.Linear(4, 128)
-        self.fc2 = nn.Linear(128, 3)
-
+        self.num_features = num_features
+        self.num_actions = num_actions
+        self.hidden_size = hidden_size
+        self.layer1 = nn.Linear(num_features, hidden_size)
+        self.layer2 = nn.Linear(hidden_size, num_actions)
+        self.softmax = nn.Softmax(dim=1)
+        
     def forward(self, x):
-        x = torch.Tensor(x)
-        x = torch.relu(self.fc1(x))
-        x = self.fc2(x)
-        return torch.softmax(x, dim=0)
+        x = self.layer1(x)
+        x = torch.relu(x)
+        x = self.layer2(x)
+        x = self.softmax(x)
+        return x
+    
+    def get_action(self, state):
+        state = torch.Tensor(state).unsqueeze(0)
+        probs = self.forward(state)
+        action = torch.multinomial(probs, num_samples=1)
+        return action.item() # 0-3 중 하나
 
+# Define hyperparameters
+n_episodes = 5000
+n_steps_per_episode = 1000
+learning_rate = 0.1
+num_features = 4
+num_actions = 3
+hidden_size = 10
 
-def choose_action(policy, state):
-    state = torch.Tensor(state)
-    action_probs = policy.forward(state)
-    action = np.random.choice(range(policy.n_actions),
-                              p=action_probs.detach().numpy())
-    return action
+# Initialize bandit and policy
+bandit = ContextualBandit()
+policy = Policy(num_features, num_actions, hidden_size)
+optimizer = optim.Adam(policy.parameters(), lr=learning_rate)
 
+# Training loop
+for episode in range(n_episodes):
+    state = bandit.get_state()
+    total_reward = 0
+    for step in range(n_steps_per_episode):
+        action = policy.get_action(state) # 0-3 중 하나의 값
+        reward = bandit.pull_arm(action)
+        total_reward += reward
+        
+        # Calculate loss and update policy
+        optimizer.zero_grad()
+        
+        state_one_hot = np.zeros(n_states)
+        state_one_hot[state] = 1
+        state_one_hot = torch.Tensor(state_one_hot)
+        
+        action_one_hot = np.zeros(n_arms)
+        action_one_hot[action] = 1
+        action_one_hot = torch.Tensor(action_one_hot).unsqueeze(0)
+        
+        action_probs = policy.forward(state_one_hot)
+        action_prob = action_probs.gather(1, action_one_hot.long())
+        
+        loss = -torch.log(action_prob) * reward
+        loss.backward()
+        optimizer.step()
+        
+        state = bandit.get_state()
+        
+    # Print total reward of each episode
+    if episode % 100 == 0:
+        print("Episode {}: Total reward = {}".format(episode, total_reward))
 
-def update_policy(policy, optimizer, rewards, actions, states):
-    loss = torch.Tensor(
-        [-np.log(action_probs[a]) * r for a, r in zip(actions, rewards)])
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
-
-
-def main():
-    n_episodes = 2000
-    learning_rate = 0.01
-    bandit = ContextualBandit()
-    policy = Policy()
-    optimizer = optim.SGD(policy.parameters(), lr=learning_rate)
-
-    for episode in range(n_episodes):
-        state = bandit.state[bandit.correct_bandit]
-        action = choose_action(policy, state)
-        reward = bandit.get_reward(action)
-
-        update_policy(policy, optimizer, [reward], [action], [state])
-
-        if episode % 100 == 0:
-            print(f"Episode {episode}, Reward: {reward}")
-
-
-if __name__ == '__main__':
-    main()
+print("Testing policy...")
+for state in range(num_features):
+    action = policy.get_action(state)
+    print("State {}: Action = {}".format(state, action))
