01e1e9f53a75676647fe8a1f69246949b6cacfe7
yeop <shxoduq2@naver.com>
2023-03-10 13:22:45 +0900

    Add VPG code

--- vanilla policy gradient practice.py
+++ vanilla policy gradient practice.py
@@ -1,146 +1,72 @@
+# vanilla policy gradient는 앞의 두 bandit 문제에서 고려한 것을 포함해 총 3가지를 고려해야 함
+# 1. 액션 의존성 :MAB에서 사용된 것처럼 각각의 액션이 보상을 가져다 줄 확률은 다름
+# 2. 상태 의존성 :MAB와 달리, CB에서 각 액션을 취할 때의 보상은 그 액션을 취할 당시의 상태와 관계가 있음
+# 3. 시간 의존성 :에이전트는 보상에 대해 시간 지연된 시점에 학습함, 따라서 데이터에 샘플을 저장 후 뭉쳐서 학습 
+
+import gym
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import torch.optim as optim
-import numpy as np
-import gym
+from torch.distributions import Categorical
 
-env = gym.make('CartPole-v0')
+#Hyperparameters
+learning_rate = 0.002
+gamma         = 0.99
 
-gamma = 0.99
-
-def discount_rewards(r):
-    """ take 1D float array of rewards and compute discounted reward """
-    discounted_r = np.zeros_like(r)
-    running_add = 0
-    for t in reversed(range(0, r.size)):
-        running_add = running_add * gamma + r[t]
-        discounted_r[t] = running_add
-    return discounted_r
-
-class agent(nn.Module):
-    def __init__(self, lr, s_size, a_size, h_size):
-        super(agent, self).__init__()
-        self.lr = lr
-        #These lines established the feed-forward part of the network. The agent takes a state and produces an action.
-        self.state_in = nn.Linear(s_size, h_size)
-        self.hidden = nn.Linear(h_size, h_size)
-        self.output = nn.Linear(h_size, a_size)
-        self.softmax = nn.Softmax(dim=1)
+class Policy(nn.Module):
+    def __init__(self):
+        super(Policy, self).__init__()
+        self.data = []
         
-        #The next six lines establish the training proceedure. We feed the reward and chosen action into the network
-        #to compute the loss, and use it to update the network.
-        self.reward_holder = []
-        self.action_holder = []
-
-        self.loss = nn.CrossEntropyLoss()
-
-        self.optimizer = optim.Adam(self.parameters(), lr=self.lr)
-
+        self.fc1 = nn.Linear(4, 128)
+        self.fc2 = nn.Linear(128, 2)
+        self.optimizer = optim.Adam(self.parameters(), lr=learning_rate)
+        
     def forward(self, x):
-        x = self.state_in(x)
-        x = torch.relu(x)
-        x = self.hidden(x)
-        x = torch.relu(x)
-        x = self.output(x)
-        x = self.softmax(x)
+        x = F.relu(self.fc1(x))
+        x = F.softmax(self.fc2(x), dim=0)
         return x
-
-    def update(self, rewards, actions, states):
-        # Compute discounted rewards
-        discounted_rewards = discount_rewards(rewards)
-
-        # Convert actions to tensor
-        actions = torch.tensor(actions, dtype=torch.long)
-
-        # Convert rewards to tensor
-        rewards = torch.tensor(discounted_rewards, dtype=torch.float)
-
-        # Convert states to tensor
-        states = torch.tensor(states, dtype=torch.float)
-
-        # Compute the negative log-likelihood loss
-        logits = self(states)
-        loss = self.loss(logits, actions) * -rewards.mean()
-
-        # Compute gradients
+      
+    def put_data(self, item):
+        self.data.append(item)
+        
+    def train_net(self):
+        R = 0
         self.optimizer.zero_grad()
-        loss.backward()
-
-        # Update weights
+        for r, prob in self.data[::-1]:
+            R = r + gamma * R
+            loss = -torch.log(prob) * R
+            loss.backward()
+        # backpropagation data 개수만큼 수행한 뒤 update -> 일종의 batch 단위 수행 느낌?
         self.optimizer.step()
+        self.data = []
 
-torch.manual_seed(0) # Set the random seed for reproducibility
-
-myAgent = agent(lr=1e-2, s_size=4, a_size=2, h_size=8) # Load the agent.
-
-total_episodes = 5000 # Set total number of episodes to train agent on.
-max_ep = 999
-update_frequency = 5
-
-total_reward = []
-total_length = []
-
-for i in range(total_episodes):
-    s = env.reset()
-    running_reward = 0
-    ep_history = {'states': [], 'actions': [], 'rewards': []}
-    for j in range(max_ep):
-        # Probabilistically pick an action given our network outputs.
-        a_dist = myAgent(torch.tensor(s, dtype=torch.float))
-        a = np.random.choice(a_dist.detach().numpy(), p=a_dist.detach().numpy())
-        a = np.argmax(a_dist.detach().numpy() == a)
-        s1, r, d, _ = env.step(a) # Get our reward for taking an action given a bandit.
-        ep_history['states'].append(s)
-        ep_history['actions'].append(a)
-        ep_history['rewards'].append(r)
-        s = s1
-        running_reward += r
-
-        if d == True:
-            # Update the network
-            myAgent.update(ep_history['rewards'], ep_history['actions'], ep_history['states'])
-
-            # Reset the episode history
-            ep_history = {'states': [], 'actions': [], 'rewards': []}
-
-            # Record the total reward and episode length
-            total_reward.append(running_reward)
-            total_length.append(j)
-            break
-
-        if j == max_ep-1:
-            # Update the network
-            myAgent.update(ep_history['rewards'], ep_history['actions'], ep_history['states'])
-
-            # Reset the episode history
-            ep_history = {'states': [], 'actions': [], 'rewards': []}
-
-            # Record the total reward and episode length
-            total_reward.append(running_reward)
-            total_length.append(j)
-            break
-
-        if i % update_frequency == 0:
-            # Get the gradients from the agent
-            grads = myAgent.parameters()
-
-            # Add the gradients to the gradBuffer
-            for ix, grad in enumerate(grads):
-                gradBuffer[ix] += grad.detach().numpy()
-
-            # Update the agent's parameters with the mean of the gradients
-            for ix, grad in enumerate(gradBuffer):
-                myAgent.parameters()[ix].grad = torch.tensor(grad / update_frequency, requires_grad=True)
-
-            # Update the network
-            myAgent.optimizer.step()
-
-            # Reset the gradBuffer
-            for ix, grad in enumerate(gradBuffer):
-                gradBuffer[ix] = grad * 0
-
-    # Print the total reward every 100 episodes
-    if i % 100 == 0:
-        print("Episode: ", i, " Total Reward: ", np.mean(total_reward[-100:]))
-
-print("Training complete!")
+def main():
+    env = gym.make('CartPole-v1')
+    pi = Policy()
+    score = 0.0
+    print_interval = 100
+    
+    for n_epi in range(5000):
+        s, _ = env.reset()
+        done = False
+        
+        while not done: # CartPole-v1 forced to terminates at 500 step.
+            prob = pi(torch.from_numpy(s).float())
+            m = Categorical(prob)
+            a = m.sample()
+            s_prime, r, done, info, _ = env.step(a.item())
+            pi.put_data((r,prob[a]))
+            s = s_prime
+            score += r
+            
+        pi.train_net()
+        
+        if n_epi%print_interval==0 and n_epi!=0:
+            print("# of episode :{}, avg score : {}".format(n_epi, score/print_interval))
+            score = 0.0
+    env.close()
+    
+if __name__ == '__main__':
+    main()
